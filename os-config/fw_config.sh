dd55d52514acf5b7b2a27e783255289c051127b6
zhangkai5565 <zhangkai5565@fiberhome.com>
2019-05-29 18:02:29 +0800

    add files

--- os-config/fw_config.sh
+++ os-config/fw_config.sh
@@ -14,6 +14,11 @@ firewall-cmd --add-port=3000/tcp --permanent >/dev/null
 firewall-cmd --add-port=9090/tcp --permanent >/dev/null
 firewall-cmd --add-port=9093/tcp --permanent >/dev/null
 firewall-cmd --add-port=9100/tcp --permanent >/dev/null
+firewall-cmd --add-port=9283/tcp --permanent >/dev/null
+firewall-cmd --add-port=8443/tcp --permanent >/dev/null
+firewall-cmd --add-port=5000/tcp --permanent >/dev/null
+firewall-cmd --add-port=3260/tcp --permanent >/dev/null
+firewall-cmd --add-port=9287/tcp --permanent >/dev/null
 firewall-cmd --add-port=6789-7300/tcp --permanent >/dev/null
 
 systemctl stop firewalld
