45ad9ab381243ac9621fd5d1c084fc487c55abb9
zhangkai5565 <zhangkai5565@fiberhome.com>
2019-05-29 09:13:35 +0800

    add files

--- os-config/fw_config.sh
+++ os-config/fw_config.sh
@@ -14,6 +14,7 @@ iptables -A INPUT -p all  -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -p tcp --sport 53 -j ACCEPT
 iptables -A INPUT -p icmp --icmp-type any -j ACCEPT
 iptables -A INPUT -p tcp --dprot 22 -j ACCEPT 
+iptables -A INPUT -p tcp --dprot 3000 -j ACCEPT 
 iptables -A INPUT -p tcp --dprot 9100 -j ACCEPT 
 iptables -A INPUT -p tcp --dprot 9328 -j ACCEPT 
 iptables -A INPUT -p tcp --dprot 9090 -j ACCEPT 
