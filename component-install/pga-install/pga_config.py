fbef478abf22835c362bd79d9ddd179b9c20c320
zhangkai5565 <zhangkai5565@fiberhome.com>
2019-05-30 16:09:01 +0800

    add files

---
+++ component-install/pga-install/pga_config.py
@@ -0,0 +1,22 @@
+#!/bin/python
+#Date:2019.5.21
+#Author:zhangkai
+
+import yaml,os,sys
+
+monIp,myIp=sys.argv[1:]
+ip_tmp = monIp.split(",")
+ceph_targ=map(lambda x:x+":9283",ip_tmp)
+node_targ=map(lambda x:x+":9100",ip_tmp)
+
+nowDir=os.path.abspath(os.path.dirname(__file__))
+dstPath=os.path.join(nowDir,'conf/prometheus.yml')
+
+with open(dstPath,'r+') as f:
+  data = yaml.load(f)
+  data["alerting"]["alertmanagers"][0]["static_configs"][0]["targets"]=[myIp+":9093"]
+  data["scrape_configs"][0]["static_configs"][0]["targets"]=[myIp+":9090"]
+  data["scrape_configs"][1]["static_configs"][0]["targets"]=ceph_targ
+  data["scrape_configs"][2]["static_configs"][0]["targets"]=node_targ
+  yaml.dump(data,f)
+
--- component-install/pga-install/pga_config.sh
+++ component-install/pga-install/pga_config.sh
@@ -3,10 +3,26 @@
 #Date:2019.5.21
 #Author:zhangkai
 
-set -e
+#set -e
 
-dst_dir="/usr/local/prometheus"
+while getopts m: option
+do
+case "${option}"
+in
+m) monIp=${OPTARG};;
+esac
+done
 
-[ -f "conf/alertmanager.yml" ] && cp --force conf/alertmanager.yml $dst_dir/alertmanager/ && systemctl restart alertmanager && echo "alertmanager conf sucess" 
-[ -f "conf/prometheus.yml" ] && cp --force conf/prometheus.yml $dst_dir/prometheus/ && systemctl restart prometheus && echo "prometheus conf sucess"
-
+if [[ ! "$monIp" ]];then
+  dst_dir="/usr/local/prometheus"
+  [ -f "conf/alertmanager.yml" ] && cp --force conf/alertmanager.yml $dst_dir/alertmanager/ && systemctl restart alertmanager && echo "alertmanager conf sucess" 
+  [ -f "conf/prometheus.yml" ] && cp --force conf/prometheus.yml $dst_dir/prometheus/ && systemctl restart prometheus && echo "prometheus conf sucess"
+else
+  echo "now $monIp"
+  monIpSub=`echo $monIp|awk -F, '{print $1}'`
+  monIpSub2=`echo ${monIpSub%.*}`
+  myIp=`ip addr | grep inet | grep -v 127.0.0.1 | grep -v inet6 | awk '{print $2}' |awk -F/ '{print $1}'|grep $monIpSub2`
+  dirNow=$(cd "$(dirname "$0")";pwd)
+  [[ ! $myIp ]] &&  "*****error*****myIp get fail!"
+  python $dirNow/pga_config.py $monIp $myIp
+fi
