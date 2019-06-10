b447a80b62323df6108d190ac5cbdfaf1bffddbb
zhangkai5565 <zhangkai5565@fiberhome.com>
2019-06-10 16:53:41 +0800

    modify rsyslog.conf

--- .pre_install.sh
+++ .pre_install.sh
@@ -8,14 +8,14 @@ find . -name '.*conf' -exec sed 's/\r//g' {} \;
 
 mon_hosts=`egrep ^mon_hosts os-config/fitstor.conf|awk -F= '{print $2}'`
 # os-config
-sh os-config/fw_config.sh 
+sh os-config/fw_config.sh
 read -p "config hostname?[yes] or [no]<default> " hostNameFlag
 
 if [[ "$hostNameFlag"x = "yes"x ]];then
 {
   read -p "hostname:" hostName
   echo "hostname:$hostName"
-  [[ ! $hostName ]] || sh os-config/hostname_config.sh -n $hostName 
+  [[ ! $hostName ]] || sh os-config/hostname_config.sh -n $hostName
 }
 fi
 
@@ -25,10 +25,10 @@ if [[ "$net"x = "yes"x  ]];then
     echo "MonIp as follows:%$mon_hosts"
     read -p "public ip[]: " publicIp
     read -p "cluster ip[]: " clusterIp
-    sh os-config/network_config.sh -p $publicIp -c $clusterIp 
+    sh os-config/network_config.sh -p $publicIp -c $clusterIp
     read -p "please CTRL + C to quit...then restart network"
   }
-fi 
+fi
 # pga-install
   monIpSub=`echo $mon_hosts|awk -F, '{print $1}'`
   monIpSub2=`echo ${monIpSub%.*}`
@@ -37,8 +37,11 @@ fi
   echo "server1:$server1;server2:$server2"
   myIp=`ip addr | grep inet | grep -v 127.0.0.1 | grep -v inet6 | awk '{print $2}' |awk -F/ '{print $1}'|grep $monIpSub2`
   [[ ! $myIp ]] &&  "*****error*****get host ip fail!" && exit 1
-  
-#read -p "role [server or node]: " role
+
+# modify rsyslog.conf
+sh component-install/pga-install/rsyslog.sh
+
+#install server or node
 role=""
 if [[ "$myIp"x = "$server1"x ]] || [[ "$myIp"x = "$server2"x ]] ;then
   role="server"
@@ -50,6 +53,5 @@ else
 fi
 echo "role:$role"
 # health-check
+sleep 2
 sh health-check/pga-check.sh -r $role
-
-
