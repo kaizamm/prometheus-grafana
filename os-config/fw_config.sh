00151e43b3f0b4f04b71af1cdcc49afa2aa99cda
zhangkai5565 <zhangkai5565@fiberhome.com>
2019-06-10 14:34:00 +0800

    add files

--- os-config/fw_config.sh
+++ os-config/fw_config.sh
@@ -4,22 +4,16 @@
 setenforce 0
 sed -i "s/^SELINUX=.*/SELINUX=disabled/g" /etc/selinux/config
 
-#config firewalld 
+#config firewalld
 systemctl start firewalld
 sleep 3
 #iptables -F
 
-firewall-cmd --add-port=22/tcp --permanent >/dev/null
-firewall-cmd --add-port=3000/tcp --permanent >/dev/null
-firewall-cmd --add-port=9090/tcp --permanent >/dev/null
-firewall-cmd --add-port=9093/tcp --permanent >/dev/null
-firewall-cmd --add-port=9100/tcp --permanent >/dev/null
-firewall-cmd --add-port=9283/tcp --permanent >/dev/null
-firewall-cmd --add-port=8443/tcp --permanent >/dev/null
-firewall-cmd --add-port=5000/tcp --permanent >/dev/null
-firewall-cmd --add-port=3260/tcp --permanent >/dev/null
-firewall-cmd --add-port=9287/tcp --permanent >/dev/null
-firewall-cmd --add-port=6789-7300/tcp --permanent >/dev/null
+firewall-cmd --add-port=22/tcp --permanent
+firewall-cmd --add-port=3000/tcp --permanent
+firewall-cmd --add-port=9090/tcp --permanent
+firewall-cmd --add-port=9093/tcp --permanent
+firewall-cmd --add-port=9100/tcp --permanent
+firewall-cmd --add-port=6789-7100/tcp --permanent
 
 systemctl stop firewalld
-echo "firewalld stopped"
