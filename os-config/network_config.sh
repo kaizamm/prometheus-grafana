d2f112f2c4a0a4f87a883bb9418e46e1c7bb34cf
zhangkai5565 <zhangkai5565@fiberhome.com>
2019-06-11 14:25:37 +0800

    modify network.sh

--- os-config/network_config.sh
+++ os-config/network_config.sh
@@ -11,6 +11,7 @@ done
 
 [[ ! $publicIp ]] || [[ ! $clusterIp ]] && echo "*****[error]*****Usage:./hostname_config.sh -p publicIp -c clusterIp"
 
+date_now=`date +%Y%m%d%H%M%S`
 interface=`ls /etc/sysconfig/network-scripts/|egrep ^ifcfg-e` || echo "*****[error]*****Not find interface"
 
 networkDir='/etc/sysconfig/network-scripts'
@@ -25,25 +26,25 @@ function publicConfig()
   #public uuid && device
   publicUuid=`egrep "^UUID" $networkDir/$publicInterface | awk -F= '{print $2}'`
   publicDevice=`egrep "^DEVICE" $networkDir/$publicInterface|awk -F= '{print $2}'`
-  
-  publicSubnetType="PREFIX"                                      
-  read -p "[public subnet(eg:24)]:"  publicSubnet                 
+
+  publicSubnetType="PREFIX"
+  read -p "[public subnet(defalut:24)]:"  publicSubnet
   if [[ ! $publicSubnet ]]; then
-    publicSubnet="24" 
+    publicSubnet="24"
   fi
   publicGatewayPre=`echo ${publicIp%.*}.254`
-  read -p "[public gateway($publicGatewayPre)]:"  publicGateway
+  read -p "[public gateway(default:$publicGatewayPre)]:"  publicGateway
   [[ ! $publicGatewayPre ]] || publicGateway=$publicGatewayPre
 
   [[ ! $publicUuid ]] || [[ ! $publicDevice ]]  && echo "*****[error]*****Uid or device name get none"
   echo "*****[info]*****publicInterface:$publicInterface publicIp:$publicIp $publicSubnetType:$publicSubnet publicGateway:$publicGateway"
   read -p "confirm<Enter> cancel<ctrl+c>:"
-  mv $networkDir/$publicInterface $networkDir/bak_$publicInterface  && \
+  mv $networkDir/$publicInterface $networkDir/bak_$publicInterface$date_now  && \
   dirNow=$(cd "$(dirname "$0")";pwd)
   cp $dirNow/template/ifcfg-network $networkDir/$publicInterface || echo "*****[error]*****mv $publicInterface error"
   sleep 1
   sed -i -e  "s/^IPADDR=.*/IPADDR=$publicIp/g" -e "s/^$publicSubnetType=.*/$publicSubnetType=$publicSubnet/g" -e "s/^GATEWAY=.*/GATEWAY=$publicGateway/g" -e "s/^UUID=.*/UUID=$publicUuid/g" -e "s/^NAME=.*/NAME=$publicDevice/g" -e "s/^DEVICE=.*/DEVICE=$publicDevice/g"  $networkDir/$publicInterface
-  
+
 }
 
 function clusterConfig()
@@ -51,20 +52,20 @@ function clusterConfig()
   #cluster uuid && device
   clusterUuid=`egrep "^UUID" $networkDir/$clusterInterface | awk -F= '{print $2}'`
   clusterDevice=`egrep "^DEVICE" $networkDir/$clusterInterface | awk -F= '{print $2}'`
-  clusterSubnetType="PREFIX"                                      
-  read -p "[cluster subnet(24)]:"  clusterSubnet              
+  clusterSubnetType="PREFIX"
+  read -p "[cluster subnet(24)]:"  clusterSubnet
   if [[ ! $clusterSubnet ]];then
     clusterSubnet="24"
   fi
   clusterGatewayPre=`echo ${clusterIp%.*}.254`
-  read -p "[cluster gateway($clusterGatewayPre)]:"  clusterGateway
+  read -p "[cluster gateway(default:$clusterGatewayPre)]:"  clusterGateway
   [[ ! $clusterGatewayPre ]] || clusterGateway=$clusterGatewayPre
-  
+
   [[ ! $clusterUuid ]] || [[ ! $clusterDevice ]] && echo "*****[error]*****Uid or device name get none"
   echo "*****[info]*****clusterInterface:$clusterInterface clusterIp:$clusterIp $clusterSubnetType:$clusterSubnet  clusterGateway:$clusterGateway"
 
   read -p "confirm<Enter> cancel<ctrl+c>:"
-  mv $networkDir/$clusterInterface $networkDir/bak_$clusterInterface  && \
+  mv $networkDir/$clusterInterface $networkDir/bak_$clusterInterface$date_now  && \
   dirNow=$(cd "$(dirname "$0")";pwd)
   cp $dirNow/template/ifcfg-network $networkDir/$clusterInterface || echo "*****[error]*****mv $clusterInterface error"
   sed -i -e  "s/^IPADDR=.*/IPADDR=$clusterIp/g" -e "s/^$clusterSubnetType=.*/$clusterSubnetType=$clusterSubnet/g" -e "s/GATEWAY=.*/GATEWAY=$clusterGateway/g" -e "s/^UUID=.*/UUID=$clusterUuid/g" -e "s/^NAME=.*/NAME=$clusterDevice/g" -e "s/^DEVICE=.*/DEVICE=$clusterDevice/g"  $networkDir/$clusterInterface
@@ -76,7 +77,8 @@ publicConfig
 if [ "$publicInterface"x = "$clusterInterface"x ];then
   echo "[*****warning*****public interace = cluster interface, just config public interace only"
   echo "*****[sucess]*****modify network,please confirm $networkDir/$publicInterface ,and systemctl restart network"
-  exit 0 
+  read -p "please CTRL + C to quit...then restart network"
+  exit 0
 fi
 
 clusterConfig
